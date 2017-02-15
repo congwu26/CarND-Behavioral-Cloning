4b471e1f920a7b2a3424d7190a96a6c70589cbfa
Cong Wu <wucong@wucongdeMacBook-Pro.local>
2017-02-15 11:34:29 -0800

    First init after training the model

--- model.py
+++ model.py
@@ -1,40 +1,31 @@
 import itertools
 import tensorflow as tf
 import numpy as np
+from scipy import misc
+import cv2
+import math
+import json
 from keras.layers import Input, Flatten, Dense
 from keras.models import Model
-from scipy import misc
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Lambda, MaxPooling2D, Activation
 from keras.layers.convolutional import Convolution2D
 from keras.layers.advanced_activations import ELU
 from keras.optimizers import Adam
-import cv2
-import math
-import json
-import matplotlib.pyplot as plt
-
-
 from sklearn.utils import shuffle
 
 
 def load_data():
-    # Get center, left, and right pics
-    center_pics = np.genfromtxt('driving_log.csv', delimiter=',', usecols=(0,), unpack=True, dtype=str,skip_header = 1) 
-    left_pics = np.genfromtxt('driving_log.csv', delimiter=',', usecols=(1,), unpack=True, dtype=str, skip_header=1)
-    right_pics = np.genfromtxt('driving_log.csv', delimiter=',', usecols=(2,), unpack=True, dtype=str, skip_header=1)
-    steering_angle = np.genfromtxt('driving_log.csv', delimiter=',', usecols=(3,), unpack=True, dtype=str,skip_header=1)
-
-    X_center = center_pics
-    X_left = left_pics
-    X_right = right_pics
-    y_train = steering_angle
+    X_center = np.genfromtxt('driving_log.csv', delimiter=',', usecols=(0,), unpack=True, dtype=str,skip_header = 1)
+    X_left = np.genfromtxt('driving_log.csv', delimiter=',', usecols=(1,), unpack=True, dtype=str, skip_header=1)
+    X_right = np.genfromtxt('driving_log.csv', delimiter=',', usecols=(2,), unpack=True, dtype=str, skip_header=1)
+    y_train = np.genfromtxt('driving_log.csv', delimiter=',', usecols=(3,), unpack=True, dtype=str,skip_header=1)
                 
     X_center, X_left, X_right, y_train = shuffle(X_center, X_left, X_right, y_train)
     y_train = y_train.astype(np.float)
                         
-    center_train, center_val, left_train, left_val, right_train, right_val, y_train, y_val = train_test_split(X_center,X_left,X_right,y_train,test_size=0.10,random_state=832289)
+    center_train, center_val, left_train, left_val, right_train, right_val, y_train, y_val = train_test_split(X_center,X_left,X_right,y_train,test_size=0.10,random_state=920426)
     
     return center_train, center_val, left_train, left_val, right_train, right_val, y_train, y_val
 
@@ -86,9 +77,10 @@ def get_model():
     return model
 
 def crop_resize(image):
-    #Resize image to 64x64 and crop top and bottom 20% to make interpretation easier for model
+    #Crop top and bottom 20% to focus training on the road
     shape = image.shape
     image = image[int(shape[0] * 0.2):int(shape[0] * 0.8), 0:shape[1]]
+    #Resize image to 64x64
     image = cv2.resize(image, (64, 64), interpolation=cv2.INTER_AREA)
     
     return image
@@ -99,63 +91,68 @@ def random_brightness(image):
     bright_image = cv2.cvtColor(bright_image,cv2.COLOR_RGB2HSV)
     bright_image[:,:,2] = bright_image[:,:,2]*(.25+np.random.uniform())
     bright_image= cv2.cvtColor(bright_image,cv2.COLOR_HSV2RGB)
+
     return bright_image
 
-def get_generator(train_features, left_features, right_features, train_labels, batch_size):
-    # Yield batch at a time to fit_generator using random data. Data will be chosen randomly between center, left, and right
-    # images, and steering angle and images will be augmented
-    images = np.zeros((batch_size, 64, 64, 3))
-    angles = np.zeros(batch_size)
-    while 1:
-        for i in range(batch_size):
-            # Generate random index value from batch
-            rand_line = np.random.randint(len(train_labels))
-            # Get a batch of training features and labels.
-            image, label = get_rand(train_features[rand_line], left_features[rand_line], right_features[rand_line], train_labels[rand_line])
-            if label != 0:
-                # Random flip image if it is not at 0 angle
-                image, label = flip(image, label)
-
-            images[i] = image
-            angles[i] = label
-        yield images, angles
-
 def get_rand(center_pic, left_pic, right_pic, angle):
-    # Randomly chooses between center, left, or right picture and adjust steering angle
-    
+    # Randomly chooses from center, left, or right camera and adjust steering angle by +- 0.3
     rand_pic = np.random.randint(3)
     if (rand_pic == 0):
-        path = left_pic.strip()
+        image = cv2.imread(left_pic.strip())
         angle += 0.3
     if (rand_pic == 1):
-        path = center_pic.strip()
+        image = cv2.imread(center_pic.strip())
     if (rand_pic == 2):
-        path = right_pic.strip()
-        angle -= 0.275
-    # Apply brightness and image shifting augmentation techniques
-    image = cv2.imread(path)
+        image = cv2.imread(right_pic.strip())
+        angle -= 0.3
+    # Apply a random brightness ajusting and crop image to 64X64
     image = random_brightness(image)
     image = crop_resize(image)
     return image, angle
 
 
-def flip(image, label):
+def flip(image, angle):
     # Randomly flips image and negates steering angle
     ind_flip = np.random.randint(2)
     if ind_flip == 0:
         image = cv2.flip(image, 1)
-        label = - label
-    return image, label
+        angle = - angle
+    return image, angle
+
+def get_generator(center_images, left_images, right_images, labels, batch_size):
+    # Yield batch at a time to fit_generator using random data from traning dataset
+    images = np.zeros((batch_size, 64, 64, 3))
+    angles = np.zeros(batch_size)
+    while 1:
+        for i in range(batch_size):
+            # Generate random index of training dataset
+            rand_line = np.random.randint(len(labels))
+            # Grab the image and label from training dataset with random generated index
+            # And apply to get_rand function to choose one random pic from center, left or right camera
+            image, angle = get_rand(center_images[rand_line], left_images[rand_line], right_images[rand_line], labels[rand_line])
+            # Avoid zero steering angle scenario for training
+            if angle != 0:
+                # Random flip image if it is not at 0 angle
+                image, angle = flip(image, angle)
+
+            images[i] = image
+            angles[i] = angle
+        yield images, angles
+
 
 def main():
+    # Load data from IMG folder and driving_logging.csv
     center_train, center_val, left_train, left_val, right_train, right_val, y_train, y_val = load_data()
     
+    # generate keras training model
     model = get_model()
     
-    threshold = 1
+    # Training parameters
     batch_size = 512
+    epochs = 14
     
-    for i in range(14):
+    # Training loop
+    for i in range(epochs):
         # Obtain training and validation generators and use fit_generator to test data. Incrementally reduce threshold
         # to eliminate extreme values
         train_r_generator = get_generator(center_train, left_train, right_train, y_train, batch_size)
